92a601cdafd1ab1097e36e87987a11a233fd6ac7
Bunnycxk <986826561@qq.com>
2025-12-04 16:37:53 +0800

    refactor: add g_evt_fields, support on_init

--- monitor/src/lua_runtime.c
+++ monitor/src/lua_runtime.c
@@ -1,3 +1,5 @@
+#include "lua_runtime.h"
+
 #include <stddef.h>
 #include <string.h>
 #include <stdio.h>
@@ -6,7 +8,6 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
-#include "lua_runtime.h"
 
 static lua_State *g_L = NULL;
 static char g_script_buf[8192];
@@ -24,6 +25,7 @@ int lua_load_script(const char *path) {
 
     fclose(fp);
     g_script_buf[size] = '\0';
+    return 0;
 }
 
 void lua_runtime_init(void) {
@@ -41,39 +43,94 @@ void lua_runtime_init(void) {
 void decode_event(const struct nod_event_hdr *hdr, struct lua_event *evt) {
     evt->type = g_event_info[hdr->type].name;
     evt->tid = hdr->tid;
+    evt->ts = hdr->ts;
 }
 
-void push_event_to_lua(lua_State *L, const struct lua_event *evt)
+void update_global_evt(lua_State *L, const struct lua_event *evt)
 {
-    // evt table
-    lua_newtable(L);
+    lua_getglobal(L, "evt");
+    if (!lua_istable(L, -1)) {
+        lua_pop(L, 1);
+        lua_newtable(L);
+        lua_setglobal(L, "evt");
+        lua_getglobal(L, "evt");
+    }
 
-    LUA_SET_STR(L, "type", evt->type);
-    LUA_SET_INT(L, "tid", evt->tid);
-    // TODO: other evt. 
+    for (size_t i = 0; i < g_evt_fields_count; i++) {
+        const evt_field_descriptor *fd = &g_evt_fields[i];
+        const void *field_ptr = (const char *)evt + fd->offset;
+
+        switch (fd->type) {
+
+        case EVT_FLD_STRING: {
+            const char *s = *(const char **)field_ptr;
+            if (s)
+                lua_pushstring(L, s);
+            else
+                lua_pushnil(L);
+            break;
+        }
+
+        case EVT_FLD_UINT32:
+            lua_pushinteger(L, (lua_Integer)(*(const uint32_t *)field_ptr));
+            break;
+
+        case EVT_FLD_UINT64:
+            lua_pushinteger(L, (lua_Integer)(*(const uint64_t *)field_ptr));
+            break;
+
+        case EVT_FLD_CHAR: {
+            char c = *(const char *)field_ptr;
+            char buf[2] = {c, '\0'};
+            lua_pushstring(L, buf);
+            break;
+        }
+
+        default:
+            lua_pushnil(L);
+            break;
+        }
+
+        lua_setfield(L, -2, fd->name); // evt[field_name] = value
+    }
+
+    lua_pop(L, 1);
 }
 
-
-void lua_on_event(const struct lua_event *evt){
+void lua_on_event(const struct lua_event *evt) {
     if (!g_L) return;
 
+    update_global_evt(g_L, evt);
+
     lua_getglobal(g_L, "on_event");
+
     if (!lua_isfunction(g_L, -1)) {
         lua_pop(g_L, 1);
         printf("can not find on_event\n");
         return;
     }
 
-    push_event_to_lua(g_L, evt);
-
-    if (lua_pcall(g_L, 1, 0, 0) != LUA_OK) {
+    if (lua_pcall(g_L, 0, 0, 0) != LUA_OK) {
         printf("[lua] on_event error: %s\n", lua_tostring(g_L, -1));
         lua_pop(g_L, 1);
     }
 }
 
+void lua_on_init() {
+    if (!g_L) return;
 
+    lua_getglobal(g_L, "on_init");
+    if (!lua_isfunction(g_L, -1)) {
+        lua_pop(g_L, 1);
+        printf("can not find on_init\n");
+        return;
+    }
 
+    if (lua_pcall(g_L, 0, 0, 0) != LUA_OK) {
+        printf("[lua] on_init error: %s\n", lua_tostring(g_L, -1));
+        lua_pop(g_L, 1);
+    }
+}
 
 static int lua_run_code(const char *code){
     
@@ -87,17 +144,14 @@ static int lua_run_code(const char *code){
         return -2;
     }
 
-    long long return_value = 0;
-    if (lua_isinteger(g_L, -1)) {
-        return_value = lua_tointeger(g_L, -1);
-    }
-
     lua_pop(g_L, 1);
     return 0;
 }
 
 int lua_run_script(const char *path)
 {
-    lua_load_script(path);
-    return lua_run_code(g_script_buf);
+    if (lua_load_script(path) < 0) return -1;
+    int ret = lua_run_code(g_script_buf);
+    lua_on_init();
+    return ret;
 }
--- monitor/src/lua_runtime.h
+++ monitor/src/lua_runtime.h
@@ -4,16 +4,35 @@
 #include <stddef.h>
 #include "events.h"
 
-#define LUA_SET_STR(L, k, v)  \
-    do { lua_pushstring(L, v); lua_setfield(L, -2, k); } while(0)
-
-#define LUA_SET_INT(L, k, v)  \
-    do { lua_pushinteger(L, v); lua_setfield(L, -2, k); } while(0)
-
 struct lua_event {
-    const char *type;
-    uint32_t tid;
+    const char *type;      // e.g. "open", "read"
+    uint32_t tid;    // thread id
+    uint32_t pid;    // process id
+    char dir;        // direction: '>' or '<'
+    uint64_t ts;     // timestamp in ns
 };
+
+typedef enum {
+    EVT_FLD_STRING,
+    EVT_FLD_UINT32,
+    EVT_FLD_UINT64,
+    EVT_FLD_CHAR
+} evt_field_type;
+
+typedef struct {
+    const char *name;
+    evt_field_type type;
+    size_t offset;
+} evt_field_descriptor;
+
+static const evt_field_descriptor g_evt_fields[] = {
+    { "type", EVT_FLD_STRING, offsetof(struct lua_event, type) },
+    { "tid",  EVT_FLD_UINT32, offsetof(struct lua_event, tid)  },
+    { "pid",  EVT_FLD_UINT32, offsetof(struct lua_event, pid)  },
+    { "dir",  EVT_FLD_CHAR,   offsetof(struct lua_event, dir)  },
+    { "ts",   EVT_FLD_UINT64, offsetof(struct lua_event, ts)   },
+};
+static const size_t g_evt_fields_count = sizeof(g_evt_fields) / sizeof(g_evt_fields[0]);
 
 void decode_event(const struct nod_event_hdr *hdr, struct lua_event *evt);
 
@@ -22,6 +41,7 @@ void lua_runtime_init(void);
 
 
 void lua_on_event(const struct lua_event *evt);
+void lua_on_init();
 int lua_run_script(const char *path);
 
 
