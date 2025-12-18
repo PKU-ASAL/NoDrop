7be4254e41103d8e159610fa1fc5c2f311317eb4
Kidney0502 <164594136+Kidney0502@users.noreply.github.com>
2025-12-18 10:08:29 +0800

    feat: change default from start to stop

--- kmodule/trace.c
+++ kmodule/trace.c
@@ -14,20 +14,21 @@
 #include "procinfo.h"
 
 #ifndef CONFIG_HAVE_SYSCALL_TRACEPOINTS
- #error The kernel must have HAVE_SYSCALL_TRACEPOINTS in order to be useful
+#error The kernel must have HAVE_SYSCALL_TRACEPOINTS in order to be useful
 #endif
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 35))
-    #define TRACEPOINT_PROBE_REGISTER(p1, p2) tracepoint_probe_register(p1, p2)
-    #define TRACEPOINT_PROBE_UNREGISTER(p1, p2) tracepoint_probe_unregister(p1, p2)
-    #define TRACEPOINT_PROBE(probe, args...) static void probe(args)
+#define TRACEPOINT_PROBE_REGISTER(p1, p2) tracepoint_probe_register(p1, p2)
+#define TRACEPOINT_PROBE_UNREGISTER(p1, p2) tracepoint_probe_unregister(p1, p2)
+#define TRACEPOINT_PROBE(probe, args...) static void probe(args)
 #else
-    #define TRACEPOINT_PROBE_REGISTER(p1, p2) tracepoint_probe_register(p1, p2, NULL)
-    #define TRACEPOINT_PROBE_UNREGISTER(p1, p2) tracepoint_probe_unregister(p1, p2, NULL)
-    #define TRACEPOINT_PROBE(probe, args...) static void probe(void *__data, args)
+#define TRACEPOINT_PROBE_REGISTER(p1, p2) tracepoint_probe_register(p1, p2, NULL)
+#define TRACEPOINT_PROBE_UNREGISTER(p1, p2) tracepoint_probe_unregister(p1, p2, NULL)
+#define TRACEPOINT_PROBE(probe, args...) static void probe(void *__data, args)
 #endif
 
-struct nod_syscall_filter {
+struct nod_syscall_filter
+{
     int enable;
     int hooked;
     int (*filter)(struct nod_proc_info *, struct pt_regs *);
@@ -47,18 +48,18 @@ static struct tracepoint *tp_sched_process_exit;
 static int compat_register_trace(void *func, const char *probename, struct tracepoint *tp)
 {
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 15, 0))
-	return TRACEPOINT_PROBE_REGISTER(probename, func);
+    return TRACEPOINT_PROBE_REGISTER(probename, func);
 #else
-	return tracepoint_probe_register(tp, func, NULL);
+    return tracepoint_probe_register(tp, func, NULL);
 #endif
 }
 
 static void compat_unregister_trace(void *func, const char *probename, struct tracepoint *tp)
 {
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 15, 0))
-	TRACEPOINT_PROBE_UNREGISTER(probename, func);
+    TRACEPOINT_PROBE_UNREGISTER(probename, func);
 #else
-	tracepoint_probe_unregister(tp, func, NULL);
+    tracepoint_probe_unregister(tp, func, NULL);
 #endif
 }
 
@@ -71,7 +72,8 @@ static void compat_unregister_trace(void *func, const char *probename, struct tr
 // DEFINE_PER_CPU(struct nod_count, cct);
 
 static int
-syscall_probe(struct nod_proc_info *p, struct pt_regs *regs, long id, int force) {
+syscall_probe(struct nod_proc_info *p, struct pt_regs *regs, long id, int force)
+{
     int retval;
     long table_index;
     enum nod_event_type type;
@@ -81,7 +83,8 @@ syscall_probe(struct nod_proc_info *p, struct pt_regs *regs, long id, int force)
     // start = nod_nsecs();
 
     table_index = id - SYSCALL_TABLE_ID0;
-    if (likely(table_index >= 0 && table_index < SYSCALL_TABLE_SIZE)) {
+    if (likely(table_index >= 0 && table_index < SYSCALL_TABLE_SIZE))
+    {
         type = g_syscall_event_table[table_index].event_type;
 
         event_data.category = NODC_SYSCALL;
@@ -89,9 +92,11 @@ syscall_probe(struct nod_proc_info *p, struct pt_regs *regs, long id, int force)
         event_data.event_info.syscall_data.id = id;
 
         event_data.force = force;
-        
+
         retval = record_one_event(p, type, &event_data);
-    } else {
+    }
+    else
+    {
         retval = NOD_FAILURE_INVALID_EVENT;
     }
 
@@ -127,19 +132,21 @@ TRACEPOINT_PROBE(syscall_exit_probe, struct pt_regs *regs, long ret)
     }
 
 #ifdef NOD_TEST
-    NOD_TEST(current) {
+    NOD_TEST(current)
+    {
         return;
     }
 #endif
 
-    /* 
+    /*
      * Since some syscalls are not supported yet, those syscalls will be filtered here.
      * These codes will be removed in the release version.
-     */ 
+     */
     id = syscall_get_nr(current, regs);
 
     evt_from = nod_event_from(&p);
-    switch(evt_from) {
+    switch (evt_from)
+    {
     case NOD_RESTORE_CONTEXT:
         // start = nod_nsecs();
         nod_restore_security(p);
@@ -161,15 +168,19 @@ TRACEPOINT_PROBE(syscall_exit_probe, struct pt_regs *regs, long ret)
     case NOD_OUT:
     case NOD_CLONE:
     case NOD_SHARE:
-        if (id == __NR_clone && ret == 0) {
+        if (id == __NR_clone && ret == 0)
+        {
             // forked child process
             unsigned long clone_flags;
             syscall_get_arguments_deprecated(current, regs, 1, 1, &clone_flags);
-            if (clone_flags & CLONE_VM) {
+            if (clone_flags & CLONE_VM)
+            {
                 if (!nod_proc_acquire(NOD_SHARE, NULL, -1, current))
                     vpr_err("acquire NOD_SHARE for childed process failed\n");
-            } else {
-                /* 
+            }
+            else
+            {
+                /*
                  * If the child process has its own address space,
                  * he should inherit parent's procinfo, including buffer, load address and pkey.
                  * We mark it here and do it lazily.
@@ -177,21 +188,31 @@ TRACEPOINT_PROBE(syscall_exit_probe, struct pt_regs *regs, long ret)
                 if (!nod_proc_acquire(NOD_CLONE, NULL, -1, current))
                     vpr_err("acquire NOD_CLONE for childed process failed\n");
             }
-        } else {
-            if (id == __NR_execve || id == __NR_execveat) {
+        }
+        else
+        {
+            if (id == __NR_execve || id == __NR_execveat)
+            {
                 /*
                  * In execve(), the address space will be replaced with the new one.
                  * The original instrumented monitor will no longer exist.
                  */
-                if (p) {
+                if (p)
+                {
                     nod_init_procinfo(current, p);
-                } else {
-                    p = nod_proc_acquire(evt_from, NULL, -1, current);
-                    if (!p) break;
                 }
-            } else if (!p) {
+                else
+                {
+                    p = nod_proc_acquire(evt_from, NULL, -1, current);
+                    if (!p)
+                        break;
+                }
+            }
+            else if (!p)
+            {
                 p = nod_proc_acquire(evt_from, NULL, -1, current);
-                if (!p) break;
+                if (!p)
+                    break;
             }
             retval = syscall_probe(p, regs, id, 0);
         }
@@ -217,7 +238,8 @@ TRACEPOINT_PROBE(syscall_procexit_probe, struct task_struct *tsk)
     }
 
 #ifdef NOD_TEST
-    NOD_TEST(tsk) {
+    NOD_TEST(tsk)
+    {
         return;
     }
 #endif
@@ -228,20 +250,23 @@ TRACEPOINT_PROBE(syscall_procexit_probe, struct task_struct *tsk)
 static int
 exit_filter(struct nod_proc_info *p, struct pt_regs *regs)
 {
-    if (!p) {
+    if (!p)
+    {
         p = nod_proc_acquire(NOD_OUT, NULL, -1, current);
-        if (!p) return 0;
+        if (!p)
+            return 0;
     }
 
-    switch(p->status) {
+    switch (p->status)
+    {
     case NOD_OUT:
     case NOD_CLONE:
     case NOD_SHARE:
         if (likely(syscall_probe(p, regs, syscall_get_nr(current, regs), 1) == NOD_SUCCESS_LOAD))
             return -EAGAIN;
-        
+
         break;
-    
+
     case NOD_IN:
         nod_restore_security(p);
 
@@ -259,11 +284,13 @@ mm_range_filter(struct nod_proc_info *p, struct pt_regs *regs)
 {
     unsigned long addr, length;
 
-    if (!p) {
+    if (!p)
+    {
         return 0;
     }
 
-    switch(p->status) {
+    switch (p->status)
+    {
     case NOD_IN:
     case NOD_RESTORE_CONTEXT:
     case NOD_RESTORE_SECURITY:
@@ -272,7 +299,8 @@ mm_range_filter(struct nod_proc_info *p, struct pt_regs *regs)
     default:
         syscall_get_arguments_deprecated(current, regs, 0, 1, &addr);
         syscall_get_arguments_deprecated(current, regs, 1, 1, &length);
-        if (nod_mmap_check(addr, length)) {
+        if (nod_mmap_check(addr, length))
+        {
             vpr_warn("is trying to manipulate monitor memory %lx len %ld\n", addr, length);
             return -EINVAL;
         }
@@ -282,10 +310,11 @@ mm_range_filter(struct nod_proc_info *p, struct pt_regs *regs)
 }
 
 static long
-hook_general(SYSCALL_DEF) {
+hook_general(SYSCALL_DEF)
+{
     int ret, id;
     struct nod_proc_info *p;
-    
+
     id = syscall_get_nr(current, current_pt_regs());
 
 #if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 20)
@@ -299,30 +328,40 @@ hook_general(SYSCALL_DEF) {
     }
 
 #ifdef NOD_TEST
-    NOD_TEST(current) {
+    NOD_TEST(current)
+    {
         return syscall_filters[id].oldsyscall(SYSCALL_ARGS);
     }
 #endif
 
     ASSERT(1 == syscall_filters[id].hooked);
-    
+
     nod_event_from(&p);
 
     ret = syscall_filters[id].filter(p, current_pt_regs());
     return ret ? ret : syscall_filters[id].oldsyscall(SYSCALL_ARGS);
 }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,3,0)
-inline void nod_write_cr0(unsigned long cr0) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 3, 0)
+inline void nod_write_cr0(unsigned long cr0)
+{
     unsigned long __force_order;
-	asm volatile("mov %0,%%cr0" : "+r"(cr0), "+m"(__force_order));
+    asm volatile("mov %0,%%cr0" : "+r"(cr0), "+m"(__force_order));
 }
 #else
-#define nod_write_cr0 write_cr0 
+#define nod_write_cr0 write_cr0
 #endif
 
-#define WPOFF do { nod_write_cr0(read_cr0() & (~0x10000)); } while (0);
-#define WPON  do { nod_write_cr0(read_cr0() | 0x10000);    } while (0);
+#define WPOFF                                   \
+    do                                          \
+    {                                           \
+        nod_write_cr0(read_cr0() & (~0x10000)); \
+    } while (0);
+#define WPON                                 \
+    do                                       \
+    {                                        \
+        nod_write_cr0(read_cr0() | 0x10000); \
+    } while (0);
 
 static void
 hook_syscall(int id, int (*filter)(struct nod_proc_info *, struct pt_regs *))
@@ -330,7 +369,8 @@ hook_syscall(int id, int (*filter)(struct nod_proc_info *, struct pt_regs *))
     if (id < 0 || id >= SYSCALL_TABLE_SIZE)
         return;
 
-    if (!syscall_filters[id].hooked) {
+    if (!syscall_filters[id].hooked)
+    {
         syscall_filters[id].hooked = 1;
         WPOFF
         syscall_filters[id].oldsyscall = syscall_table[id];
@@ -349,19 +389,22 @@ unhook_syscall(int id)
 
     if (!syscall_filters[id].hooked)
         return;
-    
+
     WPOFF
     syscall_table[id] = syscall_filters[id].oldsyscall;
     WPON
 
-    syscall_filters[id].hooked = 0;
+        syscall_filters[id]
+            .hooked = 0;
     syscall_filters[id].oldsyscall = 0;
 }
 
-int trace_syscall(void) {
+int trace_syscall(void)
+{
     int ret;
 
-    if (tracepoint_registered == 1) {
+    if (tracepoint_registered == 1)
+    {
         ret = 0;
         goto out;
     }
@@ -371,13 +414,15 @@ int trace_syscall(void) {
 #else
     ret = register_trace_syscall_exit(syscall_exit_probe);
 #endif
-    if (ret) {
+    if (ret)
+    {
         pr_err("can't create the sys_exit tracepoint\n");
         goto err_syscall_exit;
     }
 
     ret = compat_register_trace(syscall_procexit_probe, "sched_process_exit", tp_sched_process_exit);
-    if (ret) {
+    if (ret)
+    {
         pr_err("can't create the sched_process_exit tracepoint\n");
         goto err_sched_procexit;
     }
@@ -393,16 +438,17 @@ int trace_syscall(void) {
 
 err_sched_procexit:
 #if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 20)
-	compat_unregister_trace(syscall_exit_probe, "sys_exit", tp_sys_exit);
+    compat_unregister_trace(syscall_exit_probe, "sys_exit", tp_sys_exit);
 #else
-	unregister_trace_syscall_exit(syscall_exit_probe);
+    unregister_trace_syscall_exit(syscall_exit_probe);
 #endif
 err_syscall_exit:
 out:
     return ret;
 }
 
-void untrace_syscall(void) {
+void untrace_syscall(void)
+{
     if (tracepoint_registered == 0)
         return;
 
@@ -434,47 +480,49 @@ static void visit_tracepoint(struct tracepoint *tp, void *priv)
 
 static int get_tracepoint_handles(void)
 {
-	for_each_kernel_tracepoint(visit_tracepoint, NULL);
-	if (!tp_sys_exit) {
-		pr_err("failed to find sys_exit tracepoint\n");
-		return -ENOENT;
-	}
-	return 0;
+    for_each_kernel_tracepoint(visit_tracepoint, NULL);
+    if (!tp_sys_exit)
+    {
+        pr_err("failed to find sys_exit tracepoint\n");
+        return -ENOENT;
+    }
+    return 0;
 }
 #else
 static int get_tracepoint_handles(void)
 {
-	return 0;
+    return 0;
 }
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,7,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0)
 #define KPROBE_LOOKUP 1
 #include <linux/kprobes.h>
 static struct kprobe kp = {
-	    .symbol_name = "kallsyms_lookup_name"
-};
+    .symbol_name = "kallsyms_lookup_name"};
 #endif
 
-
-uint64_t nod_lookup_name(const char *name) {
+uint64_t nod_lookup_name(const char *name)
+{
 #ifdef KPROBE_LOOKUP
-	typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
-	kallsyms_lookup_name_t kallsyms_lookup_name;
-	register_kprobe(&kp);
-	kallsyms_lookup_name = (kallsyms_lookup_name_t) kp.addr;
-	unregister_kprobe(&kp);
+    typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
+    kallsyms_lookup_name_t kallsyms_lookup_name;
+    register_kprobe(&kp);
+    kallsyms_lookup_name = (kallsyms_lookup_name_t)kp.addr;
+    unregister_kprobe(&kp);
 #endif
-	return kallsyms_lookup_name(name);
+    return kallsyms_lookup_name(name);
 }
 
-int tracepoint_init(void) {
+int tracepoint_init(void)
+{
     int ret;
 
     tracepoint_registered = 0;
 
     syscall_table = (sys_call_ptr_t *)nod_lookup_name("sys_call_table");
-    if (syscall_table == 0) {
+    if (syscall_table == 0)
+    {
         ret = -EINVAL;
         goto out;
     }
@@ -483,8 +531,9 @@ int tracepoint_init(void) {
     if (ret)
         goto out;
 
-    ret = trace_syscall();
-    if (ret) {
+    // ret = trace_syscall();
+    if (ret)
+    {
         goto out;
     }
 
@@ -494,6 +543,7 @@ out:
     return ret;
 }
 
-void tracepoint_destory(void) {
+void tracepoint_destory(void)
+{
     untrace_syscall();
 }
