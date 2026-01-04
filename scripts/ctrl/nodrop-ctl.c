a64a31fcfc1e9f565734bf75954bdf0f2f2f8e32
Bunnycxk <986826561@qq.com>
2026-01-04 22:41:41 +0800

    fix: fix merge error(ctrl)

--- scripts/ctrl/nodrop-ctl.c
+++ scripts/ctrl/nodrop-ctl.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
     int fd;
     int ret;
     FILE *file;
+    unsigned long bufsize;
     struct buffer_count_info cinfo;
     struct fetch_buffer_struct fetch;
     struct nod_event_statistic nod_stat;
@@ -150,6 +151,7 @@ int main(int argc, char *argv[])
             return -1;
         }
         printf("buffer size: %lu\n", bufsize);
+    }
     else
     {
         fprintf(stderr, "Unknown cmd %s\n", argv[1]);
